208a74532188f844e62b7c58d368341354ce55c7
bigstark <kkjkkdk@gmail.com>
2018-11-24 19:09:39 +0900

    edit xml code style

--- app/src/main/java/com/mashup/SplashActivity.kt
+++ app/src/main/java/com/mashup/SplashActivity.kt
@@ -8,5 +8,6 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
