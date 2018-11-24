c82b810483e8d32f3227c57aa2407a3b1e5cd24d
bigstark <kkjkkdk@gmail.com>
2018-11-25 03:29:17 +0900

    home drawer screen.

--- app/src/main/java/com/mashup/app/home/HomeActivity.kt
+++ app/src/main/java/com/mashup/app/home/HomeActivity.kt
@@ -2,10 +2,12 @@ package com.mashup.app.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mashup.R
 
 class HomeActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
     }
 }
