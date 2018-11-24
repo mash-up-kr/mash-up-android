0b861cf7ba8bd8d86ba88ef434adb12fb833b1e3
bigstark <kkjkkdk@gmail.com>
2018-11-25 04:50:43 +0900

    complete home layout

--- app/src/main/java/com/mashup/SplashActivity.kt
+++ app/src/main/java/com/mashup/SplashActivity.kt
@@ -1,12 +1,16 @@
 package com.mashup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mashup.app.home.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, HomeActivity::class.java))
     }
 }
--- app/src/main/java/com/mashup/app/home/HomeActivity.kt
+++ app/src/main/java/com/mashup/app/home/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.mashup.app.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.mashup.R
 
 class HomeActivity: AppCompatActivity() {
@@ -9,5 +11,10 @@ class HomeActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        viewModel.recentNotice.observe(this, Observer {
+
+        })
     }
 }
